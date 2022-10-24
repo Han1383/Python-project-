b0a56cdefc92bb148459301bfcf45df9ae28db1d
Han1383 <116223306+Han1383@users.noreply.github.com>
2022-10-24 11:33:21 +0330

    Add files via upload

---
+++ Ex1_1.py
@@ -0,0 +1,13 @@
+Python 3.11.0b5 (main, Jul 25 2022, 22:59:14) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def EX1_1(X):
+    '''
+
+  sum number given with 1 .
+
+  example:
+  >>>Ex1_1example(0)
+  1
+
+  '''
+    return (x+1)
