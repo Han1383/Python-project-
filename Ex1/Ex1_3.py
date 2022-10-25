741fb49a08c1057198f03b038600fc07602a2b85
Han1383 <116223306+Han1383@users.noreply.github.com>
2022-10-25 21:42:06 +0330

    Add files via upload

---
+++ Ex1/Ex1_3.py
@@ -0,0 +1,15 @@
+Python 3.11.0b5 (main, Jul 25 2022, 22:59:14) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def Ex1_3(side1,side2):
+    '''
+
+ returns the maximum sise of the third edge.
+
+example:
+>>>Ex1_3example(8,10)
+17
+
+'''
+    side1=int>0
+    side2=int>0
+    return((side1+side2)-1)
